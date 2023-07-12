3a651f2210355eb04df86f6406e22ea35888c97f
shmueldabomb441 <shmuelsternbach613@gmail.com>
2023-07-12 13:03:30 +0300

    Add more python-zmanim tests to YomiCalculatorTest

--- src/test/java/com/kosherjava/zmanim/hebrewcalendar/YomiCalculatorTest.java
+++ src/test/java/com/kosherjava/zmanim/hebrewcalendar/YomiCalculatorTest.java
@@ -3,6 +3,8 @@ package com.kosherjava.zmanim.hebrewcalendar;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.LocalDate;
+
 public class YomiCalculatorTest {
 	private static HebrewDateFormatter hdf = new HebrewDateFormatter();
 	static {
@@ -34,5 +36,66 @@ public class YomiCalculatorTest {
 		Assert.assertEquals(23, daf.getMasechtaNumber());
 		Assert.assertEquals(47, daf.getDaf());
 		System.out.println(hdf.formatDafYomiBavli(jewishCalendar.getDafYomiBavli()));
+	}
+
+	//From python-zmanim:
+
+
+	@Test
+	public void testSimpleDate() {
+		assertMatches(LocalDate.of(2017, 12, 28), 25, 30);
+	}
+
+	@Test
+	public void testBeforeCycleBegan() {
+		try { //assertThrows
+			assertMatches(LocalDate.of(1920,1,1), Integer.MIN_VALUE, Integer.MIN_VALUE);
+			assert false;//should throw
+		} catch (Throwable t) {
+			assert true;
+		}
+	}
+	@Test
+	public void testFirstDayOfCycle() {
+		assertMatches(LocalDate.of(2012, 8, 3), 0, 2);
+	}
+	@Test
+	public void testLastDayOfCycle() {
+		assertMatches(LocalDate.of(2020, 1, 4), 39, 73);
+	}
+	@Test
+	public void testBeforeShekalimTransitionEndOfShekalim() {
+		assertMatches(LocalDate.of(1969, 4, 28), 4, 13);
+	}
+	@Test
+	public void testBeforeShekalimTransitionBeginningOfYoma() {
+		assertMatches(LocalDate.of(1969, 4, 29), 5, 2);
+	}
+	@Test
+	public void testEndOfMeilah() {
+		assertMatches(LocalDate.of(2019, 10, 9), 35, 22);
+	}
+	@Test
+	public void testBeginningOfKinnim() {
+		assertMatches(LocalDate.of(2019, 10, 10), 36, 23);
+	}
+	@Test
+	public void testBeginningOfTamid() {
+		assertMatches(LocalDate.of(2019, 10, 13), 37, 26);
+	}
+	@Test
+	public void testBeginningOfMiddos() {
+		assertMatches(LocalDate.of(2019, 10, 22), 38, 35);
+	}
+	@Test
+	public void testAfterMiddos() {
+		assertMatches(LocalDate.of(2019, 10, 25), 39, 2);
+	}
+
+	private void assertMatches(LocalDate date, int masechtaNumber, int dafNum) {
+		JewishCalendar cal = new JewishCalendar(date);
+		Daf daf = cal.getDafYomiBavli();
+		Assert.assertEquals(daf.getMasechtaNumber(), masechtaNumber);
+		Assert.assertEquals(daf.getDaf(), dafNum);
 	}
 }
